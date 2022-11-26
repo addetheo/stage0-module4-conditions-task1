4ed68a24aaacd3c6baa1e58fb6eceafea6a5d29f
addetheo <56866727+addetheo@users.noreply.github.com>
2022-11-26 17:44:28 +0200

    Added implementations

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean isTrue = false;
+        if(isTrue){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean isTrue = true;
+        if(isTrue){
+            System.out.println("true");
+        }
     }
 }
